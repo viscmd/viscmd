c67d911bc50bc4b5a3bc2a08ad1bacbb54944b72
gaory1 <gaory1@qq.com>
2022-12-04 23:02:50 +0800

    feat: type blockdev and disk

--- viscmd/type.py
+++ viscmd/type.py
@@ -8,6 +8,8 @@ directory = "directory"
 netdev = 'netdev'
 netns = 'netns'
 bridge = 'bridge'
+blockdev = 'blockdev'
+disk = 'disk'
 
 
 def list_netdev():
@@ -22,8 +24,18 @@ def list_bridge():
     return subprocess.getoutput("ip -br link show type bridge | awk '{print $1}'").splitlines()
 
 
+def list_blockdev():
+    return subprocess.getoutput("lsblk -n -p -l | awk '{print $1}'").splitlines()
+
+
+def list_disk():
+    return subprocess.getoutput("lsblk --nodeps -n -p | awk '{print $1}'").splitlines()
+
+
 all_get_choices = {
     netdev: list_netdev,
     netns: list_netns,
     bridge: list_bridge,
+    blockdev: list_blockdev,
+    disk: list_disk,
 }

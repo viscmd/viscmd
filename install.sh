f1792411b1b07482809439748a6e85a90ec9c384
gaory1 <gaory1@qq.com>
2022-12-06 20:17:50 +0800

    feat: support zsh

--- install.sh
+++ install.sh
@@ -1,17 +1,45 @@
 #!/bin/bash
 
 if [ $USER != root ]; then
-  echo "Please run as root by sudo."
-  exit 1
+	echo "Please run as root by sudo."
+	exit 1
 fi
 
 if pip3 show viscmd &>/dev/null; then
-    pip3 uninstall -y viscmd
+	pip3 uninstall -y viscmd
 fi
 pip3 install .
+echo
 
-/bin/cp viscmd-completion.sh /etc/bash_completion.d/
 /bin/cp viscmd.png /usr/share/icons/
 /bin/cp viscmd.desktop /usr/share/applications/
+/bin/cp viscmd-completion-*.sh /bin/
+chmod +x /bin/viscmd-completion-*.sh
 
 test -d /var/lib/viscmd || (mkdir /var/lib/viscmd && chown $SUDO_USER /var/lib/viscmd)
+
+[[ $- == *i* ]]
+ok=false
+if [ -e /etc/bash_completion.d/ ]; then
+	ln -sf /bin/viscmd-completion-bash.sh /etc/bash_completion.d/
+	echo "bash completion installed to /etc/bash_completion.d/"
+	ok=true
+else
+	echo "WARNING: /etc/bash_completion.d/ not found. Unable to install bash completion script."
+fi
+
+zshrc=$(eval echo ~$SUDO_USER/.zshrc)
+if [ -e $zshrc ]; then
+	if ! grep -q viscmd-completion-zsh.sh $zshrc; then
+		echo >>$zshrc
+		echo '# visual command' >>$zshrc
+		echo 'source /bin/viscmd-completion-zsh.sh' >>$zshrc
+	fi
+	echo "zsh completion installed to $zshrc"
+	ok=true
+fi
+
+if $ok; then
+	echo
+	echo "OK. Please re-open the bash/zsh to take effect."
+fi
--- viscmd-completion.sh
+++
@@ -1,9 +0,0 @@
-viscmd_autocomplete() {
-    if content=$(viscmd "$READLINE_LINE"); then
-        READLINE_LINE=$content
-        READLINE_POINT=${#READLINE_LINE}
-    fi  
-}
-
-bind -x '"\e[Z": viscmd_autocomplete' 
-
